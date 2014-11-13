39f1bb4152acb5ecbf5497493c897f642c2dc559
Ratul Sarna <ratulcoding@gmail.com>
2014-11-13 22:05:38 +0530

    Done with first pass on Deque.java

--- Assignment2-Deque/src/assign2/deque/Deque.java
+++ Assignment2-Deque/src/assign2/deque/Deque.java
@@ -1,6 +1,7 @@
 package assign2.deque;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
@@ -30,25 +31,113 @@ public class Deque<Item> implements Iterable<Item> {
 	}
 	
 	public void addFirst(Item item) {
+		if (item == null) {
+			throw new NullPointerException();
+		}
 		Node<Item> newOne = new Node<>();
 		newOne.item = item;
-		if (sentinel.prev == null) {
-			sentinel.prev = newOne;
-			newOne.next = sentinel;
-		}
 		newOne.next = sentinel.next;
+		newOne.prev = sentinel;
+		sentinel.next.prev = newOne;
 		sentinel.next = newOne;
+		size += 1;
+	}
+	
+	public void addLast(Item item) {
+		if (item == null) {
+			throw new NullPointerException();
+		}
+		Node<Item> newOne = new Node<>();
+		newOne.item = item;
+		newOne.next = sentinel;
+		newOne.prev = sentinel.prev;
+		sentinel.prev.next = newOne;
+		sentinel.prev = newOne;
+		size += 1;
+	}
+	
+	public Item removeFirst() {
+		if (this.isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		Node<Item> returnNode = sentinel.next;
+		Item returnItem = returnNode.item;
+		
+		sentinel.next = sentinel.next.next;
+		sentinel.next.prev = sentinel;
+		size -= 1;
+		return returnItem;
+	}
+	
+	public Item removeLast() {
+		if (this.isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		Node<Item> returnNode = sentinel.prev;
+		Item returnItem = returnNode.item;
+		
+		sentinel.prev = sentinel.prev.prev;
+		sentinel.prev.next = sentinel;
+		size -= 1;
+		return returnItem;
 	}
 	
 	@Override
 	public Iterator<Item> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new DequeIterator();
+	}
+	
+	private class DequeIterator implements Iterator<Item> {
+		Node<Item> curr;
+
+		public DequeIterator() {
+			curr = sentinel.next;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return curr.next.prev != sentinel;
+		}
+
+		@Override
+		public Item next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			Item returnItem =curr.item;
+			curr = curr.next;
+			return returnItem;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append("[");
+		for (Item i : this) {
+			str.append(i + ", ");
+		}
+		str.append("]");
+		return str.toString();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Deque<Integer> deque = new Deque<>();
+		deque.addFirst(3);
+		deque.addFirst(2);
+		deque.addFirst(1);
+		System.out.println(deque);
+		deque.addLast(4);
+		deque.addLast(5);
+		System.out.println(deque);
+		int x = deque.removeFirst();
+		System.out.println(x);
+		System.out.println(deque);
 	}
 
 }
